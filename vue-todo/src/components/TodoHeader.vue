6741db67116286ff381ea24b3b6df0f7671d6bcd
amazingkj <jiin724@gmail.com>
2023-03-23 17:36:47 +0900

    start

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <H1>TODO it!</H1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+}
+</style>
