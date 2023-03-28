825bd89207570fb800bcd4fa0fa2b0c1ddfa5114
amazingkj <jiin724@gmail.com>
2023-03-28 15:25:25 +0900

    update module

---
+++ vue-todo/src/store/getters.js
@@ -0,0 +1,3 @@
+export const storedToDoItems = (state) => {
+        return state.todoItems;
+}
--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -1,5 +1,7 @@
 import Vue from 'vue' 
 import Vuex from 'vuex'
+import * as getters from './getters'
+import * as mutations from './mutations'
 
 Vue.use(Vuex);
 
@@ -12,6 +14,7 @@ const storage = {
                 arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
               }
             }
+
           }
           return arr;
     },    
@@ -21,30 +24,6 @@ export const store = new Vuex.Store({
     state: {
         todoItems: storage.fetch()
     },
-    getters: {
-      storedToDoItems(state) {
-        return state.todoItems;
-      }
-    },
-    mutations: {
-      addOneItem(state, todoItem){
-        const obj = {completed: false, item: todoItem};
-        localStorage.setItem(todoItem,JSON.stringify(obj));
-        state.todoItems.push(obj);
-      },
-      removeOneItem(state, payload) {
-        localStorage.removeItem(payload.todoItem.item);
-        state.todoItems.splice(payload.index, 1);
-      },
-      toggleOneItem(state, payload) {
-        //todoItem.completed = !todoItem.completed;
-        state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
-        localStorage.removeItem(payload.todoItem.item);
-        localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
-      },
-      clearAllItems(state) {
-        localStorage.clear();
-        state.todoItems = [];
-      }
-    }
+    getters,
+    mutations
 });
